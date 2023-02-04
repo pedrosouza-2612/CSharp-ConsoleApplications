388259577e2b3501e2f80718684ad60d2ef49696
pedrosouza-2612 <118398681+pedrosouza-2612@users.noreply.github.com>
2023-02-03 22:45:19 -0300

    Criação do projeto calculadora

---
+++ Calculadora/Calculadora/Operacoes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    class Operacoes
+    {
+        public static int Soma(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static int Subtrair(int x, int y)
+        {
+            return x - y;
+        }
+
+        public static double Dividir(double x, double y)
+        {
+            return x / y;
+        }
+
+        public static int Multiplicar(int x, int y)
+        {
+            return x * y;
+        }
+    }
+}
---
+++ Calculadora/Calculadora/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string operacao, confirma = "sim";
+            int x, y;
+
+            Console.Write("Digite um número: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Digite o segundo número: ");
+            y = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                Console.Write("Digite a operação que deseja: Soma, Subtração, Divisão, Multiplicação: ");
+                operacao = Console.ReadLine();
+
+                while ((operacao != "Soma") && (operacao != "Subtração") && (operacao != "Divisão") && (operacao != "Multiplicação"))
+                {
+                    Console.Write("Digite uma opção válida: ");
+                    operacao = Console.ReadLine();
+                }
+
+                if (operacao == "Soma")
+                {
+                    Console.WriteLine("A soma dos valores é: " + Operacoes.Soma(x, y));
+                }
+                else if (operacao == "Subtração")
+                {
+                    Console.WriteLine("A subtração dos valores é: " + Operacoes.Subtrair(x, y));
+                }
+                else if (operacao == "Divisão")
+                {
+                    Console.WriteLine("A divisão dos valores é: " + Operacoes.Dividir(x, y));
+                }
+                else if (operacao == "Multiplicação")
+                {
+                    Console.WriteLine("A multiplicação dos valores é: " + Operacoes.Multiplicar(x, y));
+                }
+
+                Console.Write("Deseja realizar outra operação? ");
+                confirma = Console.ReadLine();
+            } while (confirma == "sim");
+        }
+    }
+}
